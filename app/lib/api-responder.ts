b122f926ccd0498bb5bb3a0be1027baa199dac98
Ian Hodges <doctorhodges@gmail.com>
2025-03-20 21:54:27 +0000

    chore: update api endpoint

--- app/lib/api-responder.ts
+++ app/lib/api-responder.ts
@@ -29,7 +29,7 @@ interface Response {
   body: string;
 }
 
-const url = "https://apiresponder.net/info";
+const url = "https://api.apiresponder.net";
 
 export async function getAllCallsByResponderId(
   responderId: string
