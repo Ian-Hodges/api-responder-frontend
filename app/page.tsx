9706bb3268d03fdaabb002b334f43ebf77034725
Ian Hodges <doctorhodges@gmail.com>
2025-07-12 14:37:26 +0100

    feat: creat launch page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-"use client";
-
-import "./../app/app.css";
+import shortUUID from "short-uuid";
+import "./[responderId]/styles/calls.css";
+import "./[responderId]/styles/info.css";
 
 export default function App() {
-  return <h1>Nothing here</h1>;
+  return (
+    <>
+      <div className="top-bar">API Responder "?"</div>
+      <div className="layout">
+        <div className="left-sidebar">
+          <div className="auto-refresh"></div>
+        </div>
+        <div className="content">
+          <p className="info-placeholder">
+            <a href={`/${shortUUID().new()}`}>Click here to create a responder.</a>
+          </p>
+        </div>
+        <div className="right-sidebar"></div>
+      </div>
+    </>
+  );
 }
